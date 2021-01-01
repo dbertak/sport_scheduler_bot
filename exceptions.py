2afa8fef331abddffdf5e59e92b7546e879017a4
dbertak <dbertak@gmail.com>
2021-01-01 11:34:27 +0100

    removing redudancies

--- exceptions.py
+++ exceptions.py
@@ -1,20 +1,10 @@
-def send_message(context, chat_id, text, markdown=None):
-    '''Sends the error message to Telegram user'''
-
-    context.bot.send_message(
-        chat_id=chat_id,
-        text=text,
-        parse_mode=markdown
-    )
-
-
 class DatabaseNotFoundError(FileNotFoundError):
     '''To be raised when database has not been created yet.'''
 
     def __init__(self, context, chat_id, message='Database not found'):
         self.message = message
         self.text = 'Database is empty, try to create a new match first with /newmatch'
-        send_message(context, chat_id, self.text)
+        context.bot.send_message(chat_id, self.text)
         super().__init__(self.message)
 
 
@@ -24,7 +14,7 @@ class MatchNotFoundError(KeyError):
     def __init__(self, context, chat_id, match_id, message='Match not found'):
         self.message = message
         self.text = f'Your match {match_id} is not in our database, check for possible typos or create a new match with /newmatch'
-        send_message(context, chat_id, self.text)
+        context.bot.send_message(chat_id, self.text)
         super().__init__(self.message)
 
 
@@ -34,7 +24,7 @@ class SportKeyError(KeyError):
     def __init__(self, context, chat_id, sport, message='Sport not implemented yet'):
         self.message = message
         self.text = f'Unrecognized sport field {sport}: choose an available sport, find them with /showsports'
-        send_message(context, chat_id, self.text)
+        context.bot.send_message(chat_id, self.text)
         super().__init__(self.message)
 
 
@@ -44,7 +34,7 @@ class DateTimeValueError(ValueError):
     def __init__(self, context, chat_id, message='Wrong date time format'):
         self.message = message
         self.text = 'Wrong date and time format, please use dd/mm/yyyy hh:mm'
-        send_message(context, chat_id, self.text)
+        context.bot.send_message(chat_id, self.text)
         super().__init__(self.message)
 
 class EventInThePastError(ValueError):
@@ -53,5 +43,5 @@ class EventInThePastError(ValueError):
     def __init__(self, context, chat_id, message='Event in the past'):
         self.message = message
         self.text = 'Event cannot be in the past'
-        send_message(context, chat_id, self.text)
+        context.bot.send_message(chat_id, self.text)
         super().__init__(self.message)
