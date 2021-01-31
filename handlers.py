1bd9687037b54d941cb08b8116ce7199e0ff9520
dbertak <dbertak@gmail.com>
2021-01-31 22:13:40 +0000

    Newline added

--- handlers.py
+++ handlers.py
@@ -32,7 +32,7 @@ def start(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text='Hi! I am here to help you scheduling sport matches with your friends\n'
-             'Type /help for more information'
+             'Type /help for more information.\n'
              'GitHub: [SportSchedulerBot](https://github.com/dbertak/sport_scheduler_bot)',
         parse_mode=ParseMode.MARKDOWN
         )
